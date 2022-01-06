3e6ab647e2dbe1de9c25865ace5e946e82c6e29d
Rony Ormandy Ortíz Alvarez <ormandyrony@gmail.com>
2022-01-06 17:24:39 -0600

    Generación de grafica de la cola

--- cola.js
+++ cola.js
@@ -24,25 +24,59 @@ class Cola {
     }
 
     dequeue () {
-        this.primero = this.primero.siguiente;
+        if (this.queueEmpty()) {
+            this.primero = this.primero.siguiente;
+        } else {
+            console.log("La cola esta vacia");
+        }
+       
     }
 
     imprimir () {
-        let nodoAuxiliar = this.primero;
-        while (this.ultimo != nodoAuxiliar) {
-            console.log(nodoAuxiliar.dato);
-            nodoAuxiliar = nodoAuxiliar.siguiente;
+        if (this.queueEmpty()) {
+            let nodoAuxiliar = this.primero;
+            while (this.ultimo != nodoAuxiliar) {
+                console.log(nodoAuxiliar.dato);
+                nodoAuxiliar = nodoAuxiliar.siguiente;
+            }
+            console.log(this.ultimo.dato);
+        } else {
+            console.log("La cola esta vacia");
         }
-        console.log(this.ultimo.dato);
+       
     }
 
+    generarDot () {
+        let cadena = "digraph Cola { node [shape = box];\n";
+        let cadena2 = "";
+
+        let nodoAuxiliar = this.primero;
+        while (this.ultimo != nodoAuxiliar) {
+            cadena += nodoAuxiliar.dato + ";\n";
+            cadena2 += nodoAuxiliar.dato + "->" + nodoAuxiliar.siguiente.dato + ";\n";
+            nodoAuxiliar = nodoAuxiliar.siguiente;
+
+        }
+        cadena += this.ultimo.dato + ";\n";
+        cadena = cadena + cadena2 + "}";
+        console.log(cadena);
+    
+    }
+
+    queueEmpty () {
+        if (this.primero == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 let cola = new Cola();
 cola.enqueue(1);
 cola.enqueue(2);
 cola.enqueue(3);
-
+cola.generarDot();
 cola.imprimir();
 cola.dequeue();
 
